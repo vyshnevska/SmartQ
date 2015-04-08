257cf6ef4c8e90f4a77464a1fbdcc61c73c41ad3
Nina Vyshnevska <vyshnevska.n@gmail.com>
2015-04-08 13:23:46 +0300

    Add highcharts.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( full-width-pics.css user_assessments.css quizzs.css)
+Rails.application.config.assets.precompile += %w( full-width-pics.css user_assessments.css quizzs.css highcharts.js)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
